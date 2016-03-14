389824cad332726f80fa8522b4967622d7476877
cho <cho_ndi@yahoo.com>
2016-03-14 17:06:42 +0100

    modified run, manifest ...

--- wedin/run.sh
+++ wedin/run.sh
@@ -16,5 +16,6 @@ echo [$0] port is------------------------- $SERVER_PORT
 python manage.py migrate --noinput
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('cho', 'cho_ndi@yahoo.com', 'cho123')" | python manage.py shell
 
-echo [$0] Starting Django Server...
-python manage.py runserver 0.0.0.0:$SERVER_PORT --noreload
+
+echo port is $SERVER_PORT
+python manage.py runserver --noreload 0.0.0.0:$SERVER_PORT
